56674d2f52988eca2072916f50b9e8a04efc4a44
Nicola Bonomi <nicolabonomi06@gmail.com>
2025-02-24 10:26:05 +0100

    fdfdfd

--- inserisci_bolletta.php
+++ inserisci_bolletta.php
@@ -17,39 +17,38 @@
         <input type="text" id="codice_utente" name="codice_utente" required><br><br>
         <input type="submit" value="Inserisci Bolletta">
     </form>
+    <?php
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $db_nome = "utenze";
+        $tab_nome = "utenti";
+
+        // connessione al database lato server
+        $conn = new mysqli($host, $username, $password, $db_nome);
+        if ($conn->connect_errno){
+            echo "Impossibile connettersi al server: " . $conn->connect_errno . "\n";
+            exit;
+        }
+
+        // Se il form è stato inviato, inserisci i dati nel database
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $data = $_POST['data'];
+            $consumo = $_POST['consumo'];
+            $importo = $_POST['importo'];
+            $codice_utente = $_POST['codice_utente'];
+
+            $sql = "INSERT INTO bollette (Data, Consumo, Importo, codUtente) VALUES ('$data', '$consumo', '$importo', '$codice_utente')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "<p>Nuova bolletta inserita con successo</p>";
+            } else {
+                echo "Errore: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    ?>
 </body>
 </html>
-
-<?php
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $db_nome = "utenze";
-    $tab_nome = "utenti";
-
-    // connessione al database lato server
-    $conn = new mysqli($host, $username, $password, $db_nome);
-    if ($conn->connect_errno){
-        echo "Impossibile connettersi al server: " . $conn->connect_errno . "\n";
-        exit;
-    }
-
-    // Se il form è stato inviato, inserisci i dati nel database
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $data = $_POST['data'];
-        $consumo = $_POST['consumo'];
-        $importo = $_POST['importo'];
-        $codice_utente = $_POST['codice_utente'];
-
-        $sql = "INSERT INTO bollette (Data, Consumo, Importo, codUtente) VALUES ('$data', '$consumo', '$importo', '$codice_utente')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "Nuova bolletta inserita con successo";
-        } else {
-            echo "Errore: " . $sql . "<br>" . $conn->error;
-        }
-    }
-?>
 
 
 
