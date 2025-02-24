6f17757140931cc4aae349007be374e9322b5a2e
Nicola Bonomi <nicolabonomi06@gmail.com>
2025-02-24 09:44:58 +0100

    fefef

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestione DB Utenze</title>
+</head>
+<body>
+    <h1>Gestione DB Utenze</h1>
+    <ul>
+        <li><a href="inserisci_utente.php">Inserisci Nuovo Utente</a></li>
+        <li><a href="inserisci_bolletta.php">Inserisci Nuova Bolletta</a></li>
+        <li><a href="bolletteUtenze.php">Visualizza Bollette</a></li>
+        <li><a href="modifica_residenza.php">Modifica Residenza</a></li>
+    </ul>
+</body>
+</html>
