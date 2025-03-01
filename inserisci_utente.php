c2b4629a748b67ddca186be300d0df0420bff711
Leonardo Selimi <leonardoselimi06@gmail.com>
2025-03-01 10:21:11 +0100

    css utenti

--- inserisci_utente.php
+++ inserisci_utente.php
@@ -19,7 +19,7 @@
     </form>
 </body>
 </html>
-<?php\\
+<?php
 $localhost = "localhost";
 $root = "root";
 $password = "";
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO utenti (Nome, Cognome, Indirizzo, Citta) VALUES ('$nome', '$cognome', '$indirizzo', '$citta')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Nuovo utente inserito con successo";
+        echo "<p>Nuovo utente inserito con successo</p>";
     } else {
         echo "Errore: " . $sql . "<br>" . $conn->error;
     }
